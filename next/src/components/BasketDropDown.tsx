2084638bda9cb9ffaca6cabb5fc3ce6bc16efa59
OrhanMA <orhan.madi.assani@gmail.com>
2023-07-25 15:02:07 +0200

    added link on checkout button in basketdropdown

--- next/src/components/BasketDropDown.tsx
+++ next/src/components/BasketDropDown.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState, useContext } from "react";
 import { BsBasket } from "react-icons/bs";
 import BasketCart from "./ui/basketCart";
@@ -6,26 +6,17 @@ import { log } from "console";
 import { object } from "zod";
 import { Button } from "./ui/button";
 import { AppContext } from "@/components/AppContext";
-
-
-interface BasketItem {
-  name: string;
-  price: number;
-  quantity: number;
-}
+import Link from "next/link";
 
 export function BasketDropDown() {
-
   const appContext = useContext(AppContext);
   const cartItems = appContext ? appContext.cartItems : [];
-  
 
   const [cardMenuOpen, setCardMenuOpen] = useState(false);
 
   const handleBasketClick = () => {
     setCardMenuOpen(!cardMenuOpen);
   };
-  
 
   return (
     <div>
@@ -34,12 +25,13 @@ export function BasketDropDown() {
       </button>
 
       {cardMenuOpen && (
-        <div className="z-10 absolute right-0 w-[320px] mr-2 py-2 bg-white shadow-2xl rounded"
+        <div
+          className="z-10 absolute right-0 w-[320px] mr-2 py-2 bg-white shadow-2xl rounded"
           style={{
             boxShadow:
               "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px",
-          }}>
-
+          }}
+        >
           <div className=" flex justify-between items-center mt-5 mb-3 border-b border-[#A5DCE3] mx-2">
             <p className="font-bold text-[16px]">ORDER</p>
             <p className="text-[10px]">EDIT CART</p>
@@ -47,9 +39,7 @@ export function BasketDropDown() {
 
           {cartItems !== null && cartItems.length > 0 ? (
             <>
-              
               {cartItems.map((item, index) => (
-
                 <BasketCart
                   key={index}
                   name={item.name}
@@ -57,15 +47,19 @@ export function BasketDropDown() {
                   quantity={item.quantity}
                 />
               ))}
-    
+
               {/* Checkout button and price info */}
               <div className="pt-6">
                 <div className="flex justify-between items-center mx-2">
-                  <p className="text-[14px] font-bold text-[#444646]">Subtotal:</p>
+                  <p className="text-[14px] font-bold text-[#444646]">
+                    Subtotal:
+                  </p>
                   <p className="text-[15px] font-bold text-[#6D706F]">$10.00</p>
                 </div>
                 <div className="flex justify-between items-center mx-2">
-                  <p className="text-[14px] font-bold text-[#444646]">Tax (20%):</p>
+                  <p className="text-[14px] font-bold text-[#444646]">
+                    Tax (20%):
+                  </p>
                   <p className="text-[15px] font-bold text-[#6D706F]">$10.00</p>
                 </div>
                 <div className="flex justify-between items-center mx-2 mb-4">
@@ -74,16 +68,14 @@ export function BasketDropDown() {
                 </div>
               </div>
               <div className="px-2 mt-2">
-                <Button className="w-full">CHECKOUT</Button>
+                <Link href={"/cart"}>
+                  <Button className="w-full">CHECKOUT</Button>
+                </Link>
               </div>
-
-
-
             </>
-          ) : ( <p className="px-4 py-2 text-gray-800">Your cart is empty</p> )}
-
-
-          
+          ) : (
+            <p className="px-4 py-2 text-gray-800">Your cart is empty</p>
+          )}
         </div>
       )}
     </div>
