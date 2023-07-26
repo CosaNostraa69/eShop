24f0e3dd7b4effcd4f014acbc438753d05ecf467
OrhanMA <orhan.madi.assani@gmail.com>
2023-07-26 12:04:27 +0200

    optimized homepage; added checkout form in cart page

--- next/src/app/cart/page.tsx
+++ next/src/app/cart/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import {
   Table,
@@ -13,6 +13,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import axios from "axios";
+import { CheckoutForm } from "@/components/CheckoutForm";
 
 interface BasketItem {
   name: string;
@@ -23,6 +24,12 @@ interface Promotion {
   code: string;
   value: number;
   category: string;
+}
+
+interface CheckoutProps {
+  finalPrice: number;
+  usedPromotion: Promotion | null;
+  cartDataArray: BasketItem[];
 }
 const BASE_URL = "http://localhost:8000";
 const api = axios.create({
@@ -66,6 +73,9 @@ export default function Page() {
   const [isPromoCodeValid, setPromoCodeValid] = useState(false);
   const [discountedTotalPrice, setDiscountedTotalPrice] = useState(0);
   const [appliedDiscount, setAppliedDiscount] = useState(0);
+  const [appliedPromotion, setAppliedPromotion] = useState<Promotion | null>(
+    null
+  ); // New state for applied promotion
 
   const handlePromoCodeChange = (event: any) => {
     setPromoCode(event.target.value);
@@ -84,18 +94,29 @@ export default function Page() {
       setDiscountedTotalPrice(newTotalPrice);
       setAppliedDiscount(discountPercentage);
       setPromoCodeValid(true);
+      setAppliedPromotion(validPromo);
     } else {
       setDiscountedTotalPrice(0);
       setAppliedDiscount(0);
       setPromoCodeValid(false);
+      setAppliedPromotion(null);
     }
   };
 
+  // console.log(appliedPromotion);
+  const ref = useRef<null | HTMLElement>(null);
+
+  const handleClick = () => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <div className="flex flex-col items-center gap-6 py-12">
       <h1 className="text-3xl lg:text-4xl w-full justify-start p-6 font-bold">
         Cart
       </h1>
+      <h2 className="text-2xl lg:text-3xl w-full justify-start p-6 font-bold">
+        1. Recap
+      </h2>
       <div className="w-full  flex flex-col items-center gap-6 p-6 md:flex-row">
         <div className="w-full md:w-2/3 lg:w-1/2 ">
           <Table>
@@ -149,9 +170,38 @@ export default function Page() {
               Discounted total: {discountedTotalPrice}€
             </h2>
           )}
-          <Button> Checkout now</Button>
+          <Button onClick={handleClick}>Checkout now</Button>
         </div>
       </div>
+      <div className="w-full" ref={ref}>
+        <Checkout
+          finalPrice={discountedTotalPrice ? discountedTotalPrice : totalPrice}
+          usedPromotion={appliedPromotion ? appliedPromotion : null}
+          cartDataArray={cartDataArray}
+        />
+      </div>
+    </div>
+  );
+}
+
+export function Checkout({
+  finalPrice,
+  usedPromotion,
+  cartDataArray,
+}: CheckoutProps) {
+  // console.log(finalPrice);
+  // console.log(usedPromotion);
+  // console.log(cartDataArray);
+  return (
+    <div className="w-full flex flex-col items-center gap-6 py-12">
+      <h2 className="text-2xl lg:text-3xl w-full justify-start p-6 font-bold">
+        2. Checkout
+      </h2>
+      <CheckoutForm
+        finalPrice={finalPrice}
+        usedPromotion={usedPromotion}
+        cartDataArray={cartDataArray}
+      />
     </div>
   );
 }
