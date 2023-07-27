b3cbce19ede5c2f1c0eeb9b3dd83f40f1b18f6d3
OrhanMA <orhan.madi.assani@gmail.com>
2023-07-27 15:45:35 +0200

    rounded number in total prices in cart page, redirection to confirmation page when checkout succeed

--- next/src/app/confirmation/page.tsx
+++ next/src/app/confirmation/page.tsx
@@ -1,16 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { GiConfirmed } from "react-icons/gi";
 
-export default function confirmation(){
-    return(
-        <div className="w-full h-screen pt-[7%] flex justify-center text-center ">
-           
-            <div className="w-[500px] h-[300px] shadow-2xl rounded flex flex-col bg-[#E7EEE7] justify-center items-center ">
-                <GiConfirmed className="text-[60px] text-[#6BC86B] mb-2"/>
-                <h1 className="text-2xl font-bold mb-4 ">Success!</h1>
-                <p className="text-sm mb-4">A confimation has been sent to your mail. <br/> Thank you for your trust!</p>
-                <Button>Home</Button>                
-            </div>
-        </div>
-    )
+export default function confirmation() {
+  return (
+    <div className="w-full h-screen pt-[7%] flex justify-center text-center ">
+      <div className="w-[500px] h-[300px] shadow-2xl rounded flex flex-col bg-[#E7EEE7] justify-center items-center ">
+        <GiConfirmed className="text-[60px] text-[#6BC86B] mb-2" />
+        <h1 className="text-2xl font-bold mb-4 ">Success!</h1>
+        <p className="text-sm mb-4">
+          A confimation has been sent to your mail. <br /> Thank you for your
+          trust!
+        </p>
+        <Button>Home</Button>
+      </div>
+    </div>
+  );
 }
